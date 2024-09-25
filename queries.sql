e54a3f5219b5f4a9e9083e9091faf5d16a3663a5
Artem <159017621+marshmallowkot@users.noreply.github.com>
2024-09-25 21:38:27 +0300

    Delete queries.sql

--- queries.sql
+++
@@ -1,4 +0,0 @@
-select count(customer_id) as customers_count 
-from customers
---С помощью агрегируюзей финкции count мы считаем общее количество покупателей по столбцу customer_id и называем колонку customers_count 
---далее мы пишем из какой таблице взять данные

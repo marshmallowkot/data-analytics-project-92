582faf80d3f79139a9830fb1a10d6daab6655e5e
Artem <159017621+marshmallowkot@users.noreply.github.com>
2024-09-25 21:36:58 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,4 @@
+select count(customer_id) as customers_count 
+from customers
+--С помощью агрегируюзей финкции count мы считаем общее количество покупателей по столбцу customer_id и называем колонку customers_count 
+--далее мы пишем из какой таблице взять данные

a915b16c575d2cee543ac4bc24dd441fc58db5a4
Artem <159017621+marshmallowkot@users.noreply.github.com>
2024-09-26 15:03:21 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,11 @@
 --Данный запрос считает общее количество покупателей из таблицы customers
+
 select count(customer_id) as customers_count 
 from customers;
 
 --Данный запрос выводит имя и фамилию продавца, суммарную выручку с проданных товаров и количество проведенных сделок
---отсортирован по убыванию выручки
+--Отсортирован по убыванию выручки
+
 select concat(e.first_name,' ',e.last_name) as seller, 
 count(s.sales_id) as operations, floor(sum(p.price * s.quantity)) as income
 from sales s 
@@ -15,8 +17,9 @@ group by seller
 order by income desc 
 limit 10;
 
---Данный запрос информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
---отсортирован по возрастанию выручке.
+--Данный запрос выводит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
+--Отсортирован по возрастанию выручке
+
 select concat(e.first_name,' ',e.last_name) as seller, 
 floor(avg(p.price * s.quantity)) as average_income
 from sales s 
@@ -35,3 +38,22 @@ inner join products p
 on s.product_id = p.product_id
 group by seller) as avg_income_sales)
 order by average_income;
+
+--Данный запрос выводит имя и фамилию продавца, день недели и суммарную выручку
+--Отсортирован по порядковому номеру дня недели и seller
+
+with tab as (
+  select 
+		concat(first_name,' ',last_name) as seller, 
+		to_char(sale_date, 'day') as day_of_week, 
+		extract(isodow from sale_date) as dow,
+		sum(price * quantity) as income
+	from sales s 
+	inner join employees e 
+	on s.sales_person_id = e.employee_id
+	inner join products p 
+	on s.product_id = p.product_id
+	group by seller, day_of_week, dow)
+select seller, day_of_week, floor(income)
+from tab
+order by dow, seller;

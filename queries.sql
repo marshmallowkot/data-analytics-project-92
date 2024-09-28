f309143b406a7c728583aa14e0022c69b5b93542
Artem <159017621+marshmallowkot@users.noreply.github.com>
2024-09-28 22:34:10 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -21,17 +21,17 @@ limit 10;
 --за сделку по всем продавцам
 --Отсортирован по возрастанию выручке
 
-select 
-    concat(e.first_name, ' ', e.last_name) as seller, 
+select
+    concat(e.first_name, ' ', e.last_name) as seller,
     floor(avg(p.price * s.quantity)) as average_income
 from sales as s
 inner join employees as e on s.sales_person_id = e.employee_id
 inner join products as p on s.product_id = p.product_id
 group by seller
-having floor(avg(p.price * s.quantity)) < (select avg(average_income))
-from (select concat(e.first_name,' ',e.last_name) as seller,
+having floor(avg(p.price * s.quantity)) < (select avg(average_income)
+from (select concat(e.first_name, ' ', e.last_name) as seller,
 floor(avg(p.price * s.quantity)) as average_income
-from sales as s 
+from sales as s
 inner join employees as e on s.sales_person_id = e.employee_id
 inner join products as p on s.product_id = p.product_id
 group by seller) as avg_income_sales
@@ -41,31 +41,30 @@ order by average_income;
 --Отсортирован по порядковому номеру дня недели и seller
 
 with tab as (
-	select 
-		concat(first_name,' ',last_name) as seller, 
-		to_char(sale_date, 'day') as day_of_week, 
-		extract(isodow from sale_date) as dow,
-		sum(price * quantity) as income
-	from sales as s 
-	inner join employees as e 
-	on s.sales_person_id = e.employee_id
-	inner join products as p 
-	on s.product_id = p.product_id
-	group by seller, day_of_week, dow
+    select
+        concat(e.first_name, ' ', e.last_name) as seller,
+	to_char(s.sale_date, 'day') as day_of_week,
+	extract(isodow from sale_date) as dow,
+	sum(p.price * s.quantity) as income
+    from sales as s
+    inner join employees as e on s.sales_person_id = e.employee_id
+    inner join products as p on s.product_id = p.product_id
+        group by seller, day_of_week, dow
 )
 select seller, day_of_week, floor(income) as income
 from tab
 order by dow, seller;
 
---Данный запрос выводит количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
+--Данный запрос выводит количество покупателей в разных возрастных группах: 
+--16-25, 26-40 и 40+
 --Отсортирован по возрастным группам
 
 with tab as(
-	select (case 
-		when age between 16 and 25 then '16-25'
-     		when age between 26 and 40 then '26-40'
-     		when age > 40 then '40+' end) as age_category 
-     	from customers c
+    select (case
+	when age between 16 and 25 then '16-25'
+     	when age between 26 and 40 then '26-40'
+     	when age > 40 then '40+' end) as age_category
+    from customers as c
 )
 select age_category, count(age_category) as age_count
 from tab
@@ -76,52 +75,54 @@ order by age_category;
 --Отсортирован по дате по возрастанию
 
 with tab as (
-	select to_char(sale_date, 'YYYY-MM') as date,
-        customer_id,
-        sum(quantity * price) as income
-	from  employees 
-	left join sales s 
-	on employee_id = s.sales_person_id  
-	left join products p 
-	on s.product_id = p.product_id
-	where to_char(sale_date, 'YYYY-MM') is not null
+    select 
+        to_char(s.sale_date, 'YYYY-MM') as date,
+	s.customer_id,
+        sum(s.quantity * p.price) as income
+    from  employees as e
+    left join sales as s on e.employee_id = s.sales_person_id
+    left join products as p on s.product_id = p.product_id
+	where to_char(s.sale_date, 'YYYY-MM') is not null
   	group by date, customer_id
 )
-select distinct date as selling_month, 
+select distinct date as selling_month,
 count(customer_id) as total_customers,
 floor(sum(income)) as income
 from tab
 group by date
 order by date;
 
---Данный запрос выводит таблицу с покупателями, первая покупка которых пприходилась на время проведения акции
+--Данный запрос выводит таблицу с покупателями, первая покупка которых 
+--пприходилась на время проведения акции
 --Акционные товары отпускали со стоимостью равной 0
 --Отсортирован по id покупателя
 
 with tab as (
-	select concat(c.first_name,' ',c.last_name) as customer,
-	min(sale_date) as sale_date,
-    	sum(price * quantity)
-    	from customers as c
-    	left join sales as s on c.customer_id = s.customer_id
-    	left join products as p on s.product_id = p.product_id
-    	group by customer
-    	having sum(price * quantity) = 0
+    select 
+	concat(c.first_name, ' ' ,c.last_name) as customer,
+	min(s.sale_date) as sale_date,
+    	sum(p.price * s.quantity)
+    from customers as c
+    left join sales as s on c.customer_id = s.customer_id
+    left join products as p on s.product_id = p.product_id
+        group by customer
+    	having sum(p.price * s.quantity) = 0
 ), tab2 as (
-	select concat(c.first_name,' ',c.last_name) as customer,
-	min(sale_date) as sale_date,
-    	concat(e.first_name,' ',e.last_name) as seller
-    	from sales as s
-    	left join customers as c on s.customer_id = c.customer_id
-    	left join employees as e on e.employee_id = s.sales_person_id
+    select 
+	concat(c.first_name, ' ', c.last_name) as customer,
+	min(s.sale_date) as sale_date,
+    	concat(e.first_name, ' ', e.last_name) as seller
+    from sales as s
+    left join customers as c on s.customer_id = c.customer_id
+    left join employees as e on e.employee_id = s.sales_person_id
     	group by customer, seller
 )
-select tab.customer, tab.sale_date, seller
+select tab.customer, tab.sale_date, tab2.seller
 from tab
-inner join tab2 
-on tab.customer = tab2.customer 
+inner join tab2
+on tab.customer = tab2.customer
 and tab.sale_date = tab2.sale_date
-group by tab.customer, tab.sale_date, seller
+group by tab.customer, tab.sale_date, tab2.seller
 order by customer;
     
     

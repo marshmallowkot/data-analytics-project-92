03c3048ab9d5fe42af910d284891e2b75aa2aed9
Artem <159017621+marshmallowkot@users.noreply.github.com>
2024-09-28 21:36:43 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -17,26 +17,24 @@ order by income desc
 limit 10;
 
 --Данный запрос выводит информацию о продавцах, 
---чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
+--чья средняя выручка за сделку меньше средней выручки 
+--за сделку по всем продавцам
 --Отсортирован по возрастанию выручке
 
-select concat(e.first_name,' ',e.last_name) as seller, 
-floor(avg(p.price * s.quantity)) as average_income
-from sales s 
-inner join employees e 
-on s.sales_person_id = e.employee_id
-inner join products p 
-on s.product_id = p.product_id
+select 
+    concat(e.first_name, ' ', e.last_name) as seller, 
+    floor(avg(p.price * s.quantity)) as average_income
+from sales as s 
+inner join employees as e on s.sales_person_id = e.employee_id
+inner join products as p on s.product_id = p.product_id
 group by seller
-having floor(avg(p.price * s.quantity)) < (select avg(average_income) from 
-(select concat(e.first_name,' ',e.last_name) as seller, 
+having floor(avg(p.price * s.quantity)) < (select avg(average_income) 
+from (select concat(e.first_name,' ',e.last_name) as seller, 
 floor(avg(p.price * s.quantity)) as average_income
-from sales s 
-inner join employees e 
-on s.sales_person_id = e.employee_id
-inner join products p 
-on s.product_id = p.product_id
-group by seller) as avg_income_sales)
+from sales as s 
+inner join employees as e on s.sales_person_id = e.employee_id
+inner join products as p on s.product_id = p.product_id
+group by seller) as avg_income_sales
 order by average_income;
 
 --Данный запрос выводит имя и фамилию продавца, день недели и суммарную выручку

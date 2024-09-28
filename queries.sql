d7738cbe7b82a3bfbb181bf5bc20de230f453640
Artem <159017621+marshmallowkot@users.noreply.github.com>
2024-09-28 23:14:02 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -28,16 +28,16 @@ from sales as s
 inner join employees as e on s.sales_person_id = e.employee_id
 inner join products as p on s.product_id = p.product_id
 group by seller
-having 
+having
     floor(avg(p.price * s.quantity)) < (select avg(average_income)
-from (
-    select 
-        concat(e.first_name, ' ', e.last_name) as seller,
-        floor(avg(p.price * s.quantity)) as average_income
-    from sales as s
-inner join employees as e on s.sales_person_id = e.employee_id
-inner join products as p on s.product_id = p.product_id
-group by seller) as avg_income_sales)
+        from (
+            select
+                concat(e.first_name, ' ', e.last_name) as seller,
+        	floor(avg(p.price * s.quantity)) as average_income
+    	    from sales as s
+   	    inner join employees as e on s.sales_person_id = e.employee_id
+	    inner join products as p on s.product_id = p.product_id
+	    group by seller) as avg_income_sales)
 order by average_income;
 
 --Данный запрос выводит имя и фамилию продавца, день недели и суммарную выручку
@@ -46,13 +46,13 @@ order by average_income;
 with tab as (
     select
         concat(e.first_name, ' ', e.last_name) as seller,
-	to_char(s.sale_date, 'day') as day_of_week,
-        extract(isodow from sale_date) as dow,
+        to_char(s.sale_date, 'day') as day_of_week,
+        extract(isodow from s.sale_date) as dow,
 	sum(p.price * s.quantity) as income
     from sales as s
     inner join employees as e on s.sales_person_id = e.employee_id
     inner join products as p on s.product_id = p.product_id
-        group by seller, day_of_week, dow
+    group by seller, day_of_week, dow
 )
 select seller, day_of_week, floor(income) as income
 from tab
@@ -62,12 +62,12 @@ order by dow, seller;
 --16-25, 26-40 и 40+
 --Отсортирован по возрастным группам
 
-with tab as(
+with tab as (
     select (case
-	when age between 16 and 25 then '16-25'
+        when age between 16 and 25 then '16-25'
      	when age between 26 and 40 then '26-40'
      	when age > 40 then '40+' end) as age_category
-    from customers as c
+    from customers
 )
 select age_category, count(age_category) as age_count
 from tab
@@ -78,19 +78,20 @@ order by age_category;
 --Отсортирован по дате по возрастанию
 
 with tab as (
-    select 
+    select
         to_char(s.sale_date, 'YYYY-MM') as date,
 	s.customer_id,
         sum(s.quantity * p.price) as income
-    from  employees as e
+    from employees as e
     left join sales as s on e.employee_id = s.sales_person_id
     left join products as p on s.product_id = p.product_id
-	where to_char(s.sale_date, 'YYYY-MM') is not null
-  	group by date, customer_id
+    where to_char(s.sale_date, 'YYYY-MM') is not null
+    group by date, s.customer_id
 )
-select distinct date as selling_month,
-count(customer_id) as total_customers,
-floor(sum(income)) as income
+select 
+    distinct date as selling_month,
+    count(customer_id) as total_customers,
+    floor(sum(income)) as income
 from tab
 group by date
 order by date;
@@ -101,14 +102,14 @@ order by date;
 --Отсортирован по id покупателя
 
 with tab as (
-    select 
-	concat(c.first_name, ' ' ,c.last_name) as customer,
+    select
+	concat(c.first_name, ' ', c.last_name) as customer,
 	min(s.sale_date) as sale_date,
     	sum(p.price * s.quantity)
     from customers as c
     left join sales as s on c.customer_id = s.customer_id
     left join products as p on s.product_id = p.product_id
-        group by customer
+    group by customer
     	having sum(p.price * s.quantity) = 0
 ), tab2 as (
     select 
@@ -118,13 +119,14 @@ with tab as (
     from sales as s
     left join customers as c on s.customer_id = c.customer_id
     left join employees as e on e.employee_id = s.sales_person_id
-    	group by customer, seller
+    group by customer, seller
 )
 select tab.customer, tab.sale_date, tab2.seller
 from tab
 inner join tab2
-on tab.customer = tab2.customer
-and tab.sale_date = tab2.sale_date
+    on tab.customer = tab2.customer
+and 
+        tab.sale_date = tab2.sale_date
 group by tab.customer, tab.sale_date, tab2.seller
 order by customer;
     
